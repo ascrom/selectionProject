69132b67f4ab47cd99fbafa467dd4a0ef7e31166
Miguel Ascencio <mig.asc.rom@gmail.com>
2016-11-16 16:45:50 -0600

    first commit

---
+++ src/main/java/com/selectionProject/SelectionProjectApplication.java
@@ -0,0 +1,12 @@
+package com.selectionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectionProjectApplication.class, args);
+	}
+}
